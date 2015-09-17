bd59926fec7ad986c0ef613e7275a3a2b0a2698d
avomizib <bizim73@mail.ru>
2015-09-18 00:07:40 +0300

    new

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+//using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordCount = Convert.ToInt32(Console.ReadLine());
+            SortedDictionary<string, int> words = new SortedDictionary<string, int>();
+
+            for (int i = 0; i < wordCount; i++)
+            {
+                string[] ReadValue = Regex.Split(Console.ReadLine(), " ");
+                words.Add(ReadValue[0], Convert.ToInt32(ReadValue[1]));
+            }
+
+            var items = from pair in words
+                        orderby pair.Value descending
+                        select pair;
+
+            int userStringWordCount = Convert.ToInt32(Console.ReadLine());
+
+            string[] userStringWords = new string[userStringWordCount];
+
+            for (int i = 0; i < userStringWordCount; i++)
+            {
+                userStringWords[i] = Console.ReadLine();
+            }
+
+
+
+
+            //string[] ReadValue = Regex.Split(Console.ReadLine(), " ");
+            foreach (var kvp in items)
+             {
+                Console.WriteLine(kvp.Key + ": " + kvp.Value.ToString() );
+             }
+        
+            
+            
+            
+
+             //Thread.Sleep(0);
+             Console.Read(); 
+        }
+    }
+}
